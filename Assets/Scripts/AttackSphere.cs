b3ef2b92a187efc6b8025f9b74c14ab1d277c6db
Grey2508 <Grey-_@mail.ru>
2022-02-27 04:29:55 +0500

    Add take damage AttackSphere

--- Assets/Scripts/AttackSphere.cs
+++ Assets/Scripts/AttackSphere.cs
@@ -6,6 +6,8 @@ public class AttackSphere : MonoBehaviour
 {
     [SerializeField] float LifeTime = 5;
     [SerializeField] GameObject EffectPrefab;
+    [SerializeField] int DamageValue = 1;
+    
     void Start()
     {
 
@@ -14,16 +16,18 @@ public class AttackSphere : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        DestroyBullet();
+        DestroyBullet(collision.gameObject.GetComponent<PlayerTakeDamage>());
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        DestroyBullet();
+        DestroyBullet(other.attachedRigidbody.GetComponent<PlayerTakeDamage>());
     }
 
-    private void DestroyBullet()
+    private void DestroyBullet(PlayerTakeDamage playerTakeDamage)
     {
+        playerTakeDamage?.TakeDamage(DamageValue);
+
         //Instantiate(EffectPrefab, transform.position, Quaternion.identity);
 
         Destroy(gameObject);
