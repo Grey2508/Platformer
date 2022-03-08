e4a4e91cf0efcbd9edb128300fb9e4f14d5184ad
Grey2508 <Grey-_@mail.ru>
2022-03-09 01:46:03 +0500

    Add SneakyCube

---
+++ Assets/Scripts/EnemyBase/PrefabCreator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabCreator : MonoBehaviour
+{
+    public GameObject [] Prefabs;
+    public Transform Spawn;
+
+    public void Create()
+    {
+        foreach (GameObject prefab in Prefabs)
+            Instantiate(prefab, Spawn.position, Spawn.rotation);
+    }
+}
